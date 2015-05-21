eb78e251ac0f99ccf55fedff62a72f4337018564
Jon Calhoun <joncalhoun@gmail.com>
2015-05-20 23:06:44 -0400

    Resolved #6 by adding endpoint methods for answering mfas. This could use some samples still

--- lib/synapse_pay/endpoints/bank_mfa_questions_endpoint.rb
+++ lib/synapse_pay/endpoints/bank_mfa_questions_endpoint.rb
@@ -1,5 +1,16 @@
 module SynapsePay
   class BankMfaQuestionsEndpoint < APIEndpoint
+    
+    def answer(access_token, bank, mfa, params={}, headers={})
+      params = ParamsBuilder.merge({
+        :access_token => access_token,
+        :bank => bank,
+        :mfa => mfa,
+      }, params)
+      method = APIMethod.new(:post, "/bank/mfa", params, headers, self)
+      json = @client.execute(method)
+      APIList.new(:Bank, json[:banks], method, @client)
+    end
 
   end
 end
